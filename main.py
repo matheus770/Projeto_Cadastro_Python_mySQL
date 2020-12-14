db462c3fd0f6edd0232d728ed22f802fa226b9bc
NECO_MASG <63671673+matheus770@users.noreply.github.com>
2020-12-14 20:51:21 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -18,17 +18,22 @@ def cadastro():
     pcvd = tela_cadastrar.lineEdit_6.text()
 
     cursor = banco.cursor()
-    comando_SQL = "INSERT INTO cadprod (codigo, produto, custo, marca, quantidade, preçovenda) VALUES(%s, %s, %s, %s, %s, %s)"
-    dados = (str(cod),str(prod),float(cto),str(mrc),int(quant),float(pcvd))
-    cursor.execute(comando_SQL,dados)
-    banco.commit()
-    
-    tela_cadastrar.lineEdit.setText("")
-    tela_cadastrar.lineEdit_2.setText("")
-    tela_cadastrar.lineEdit_3.setText("")
-    tela_cadastrar.lineEdit_4.setText("")
-    tela_cadastrar.lineEdit_5.setText("")
-    tela_cadastrar.lineEdit_6.setText("")
+    try:
+        comando_SQL = "INSERT INTO cadprod (codigo, produto, custo, marca, quantidade, preçovenda) VALUES(%s, %s, %s, %s, %s, %s)"
+        dados = (str(cod),str(prod),float(cto),str(mrc),int(quant),float(pcvd))
+        cursor.execute(comando_SQL,dados)
+        banco.commit()
+
+        tela_cadastrar.lineEdit.setText("")
+        tela_cadastrar.lineEdit_2.setText("")
+        tela_cadastrar.lineEdit_3.setText("")
+        tela_cadastrar.lineEdit_4.setText("")
+        tela_cadastrar.lineEdit_5.setText("")
+        tela_cadastrar.lineEdit_6.setText("")
+        tela_cadastrar.label_8.setText("")
+    except:
+        tela_cadastrar.label_8.setText("Erro, codigo existente ou Campos em branco")    
+            
 
 def listagem():
     tela_cadastrar.close()
@@ -61,21 +66,68 @@ def deletar():
     iddel = idsel[produto_selecionado][0]
     cursor.execute("DELETE FROM cadprod WHERE codigo="+iddel)
 
+def abrir_att():
+     tela_att.show()
+
+def fechar_att():
+    tela_att.close()
+    tela_cadastrar.show()
+
+def atualizar_produto():
+    
+    cod = tela_att.lineEdit.text()
+    prod = tela_att.lineEdit_2.text()
+    cto = tela_att.lineEdit_3.text()
+    mrc = tela_att.lineEdit_4.text()
+    quant = tela_att.lineEdit_5.text()
+    pcvd = tela_att.lineEdit_6.text()
+
+    cursor = banco.cursor()
+    cursor.execute(f"SELECT * FROM cadprod WHERE codigo='{cod}'")
+    p = cursor.fetchall()
+    pnome = p[0][1]
+    pcusto = p[0][2]
+    pmarca = p[0][3]
+    pquant = p[0][4]
+    pvenda = p[0][5]
+    print(f"{pnome} {pcusto} {pmarca} {pquant} {pvenda}")
+    if(prod == ""):
+        prod = pnome
+    if(cto == ""):
+        cto = pcusto
+    if(mrc == ""):
+        mrc = pmarca
+    if(quant == ""):
+        quant = pquant
+    if(pcvd == ""):
+        pcvd = pvenda
+    
+    cursor.execute(f"UPDATE cadprod SET produto='{prod}', custo='{cto}', marca='{mrc}', quantidade='{quant}', preçovenda='{pcvd}' WHERE codigo='{cod}'")
+
+    tela_att.lineEdit.setText("")
+    tela_att.lineEdit_2.setText("")
+    tela_att.lineEdit_3.setText("")
+    tela_att.lineEdit_4.setText("")
+    tela_att.lineEdit_5.setText("")
+    tela_att.lineEdit_6.setText("")
+
 
 
 
 app = QtWidgets.QApplication([])
 tela_cadastrar = uic.loadUi("telacadastro.ui")
 tela_de_listagem = uic.loadUi("telalistar.ui")
-
-
+tela_att = uic.loadUi("telaatualizar.ui")
 
 tela_cadastrar.pushButton.clicked.connect(cadastro)
 tela_cadastrar.pushButton_2.clicked.connect(listagem)
-
+tela_cadastrar.pushButton_3.clicked.connect(abrir_att)
 
 tela_de_listagem.pushButton.clicked.connect(voltar_inicio)
 tela_de_listagem.pushButton_2.clicked.connect(deletar)
+
+tela_att.pushButton_2.clicked.connect(fechar_att)
+tela_att.pushButton.clicked.connect(atualizar_produto)
 
 tela_cadastrar.show()
 app.exec()
